a7314b2ad3ccd9045fdd0278d76a1654767faa58
Jordan Benner <jordanbenner@outlook.com>
2017-06-16 11:17:08 -0500

    fixes #4

--- 0001_initialize.js
+++ 0001_initialize.js
@@ -3,16 +3,3 @@ exports.up = (pgm) => {
 };
 exports.down = (pgm) => {
 };
-
-// adding migrations//
-exports.up = (pgm, run) => {
-  pgm.addColumns(
-    'reviewer',
-    {date_joined: {type: 'date'}}
-  );
-  run();
-};
-exports.down = (pgm, run) => {
-  pgm.dropColumns('reviewer', ['date_joined']);
-  run();
-};
---
+++ 0003_initialize.js
@@ -0,0 +1,12 @@
+// adding migrations//
+exports.up = (pgm, run) => {
+  pgm.addColumns(
+    'reviewer',
+    {date_joined: {type: 'date'}}
+  );
+  run();
+};
+exports.down = (pgm, run) => {
+  pgm.dropColumns('reviewer', ['date_joined']);
+  run();
+};
