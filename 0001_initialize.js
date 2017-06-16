583b79423505f3a8e582255da295c085ea3900de
Jordan Benner <jordanbenner@outlook.com>
2017-06-16 11:07:07 -0500

    fixes #1

--- 0001_initialize.js
+++ 0001_initialize.js
@@ -2,3 +2,17 @@ exports.up = (pgm) => {
 };
 exports.down = (pgm) => {
 };
+
+
+exports.up = (pgm, run) => {
+  pgm.createTable('reviewer', {
+    id: 'id',
+    name: {type: 'varchar(75)'}
+  });
+  run();
+};
+
+exports.down = (pgm, run) => {
+  pgm.dropTable('reviewer');
+  run();
+};
