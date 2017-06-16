bc7c1f6de0bacf626cb5d957d56fd327f98400e8
Jordan Benner <jordanbenner@outlook.com>
2017-06-16 11:16:15 -0500

    fixes #3

--- 0001_initialize.js
+++ 0001_initialize.js
@@ -4,20 +4,6 @@ exports.up = (pgm) => {
 exports.down = (pgm) => {
 };
 
-//
-exports.up = (pgm, run) => {
-  pgm.createTable('reviewer', {
-    id: 'id',
-    name: {type: 'varchar(75)'}
-  });
-  run();
-};
-
-exports.down = (pgm, run) => {
-  pgm.dropTable('reviewer');
-  run();
-};
-
 // adding migrations//
 exports.up = (pgm, run) => {
   pgm.addColumns(
